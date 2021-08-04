aea2d543444dd86d1a5c17e545991c43d4bdb148
Tommaso Bassignana <tommasobassignana@MacBook-Air-di-Tommaso.local>
2021-08-04 22:42:36 +0200

    changes

--- api_utils.py
+++ api_utils.py
@@ -1,3 +1,6 @@
+import sqlite3
+import pandas as pd
+
 def insertIntoSLEEP(pid ,start ,end,quality,comment):
     try:
         sqliteConnection = sqlite3.connect('main.db')
@@ -22,17 +25,17 @@ def insertIntoSLEEP(pid ,start ,end,quality,comment):
             sqliteConnection.close()
             print("The SQLite connection is closed")
             
-def insertIntoFOOD(pid ,start ,end,carbs,proteins, fats,comment):
+def insertIntoFOOD(pid ,start ,carbs,proteins, fats,comment):
     try:
         sqliteConnection = sqlite3.connect('main.db')
         cur = sqliteConnection.cursor()
         print("Connected to SQLite")
 
         sqlite_insert_with_param = """INSERT INTO MEAL
-                          (ID ,START ,END,CARBS, PROTEINS, FATS, COMMENT) 
-                          VALUES (?, ?, ?, ?, ?);"""
+                          (ID ,START ,CARBS, PROTEINS, FATS, COMMENT) 
+                          VALUES (?, ?, ?, ?,?,?);"""
 
-        data_tuple = (pid ,start ,end,carbs,proteins, fats,comment)
+        data_tuple = (pid ,start ,carbs,proteins, fats,comment)
         cur.execute(sqlite_insert_with_param, data_tuple)
         sqliteConnection.commit()
         print("insert successful")
@@ -53,8 +56,8 @@ def insertIntoGLUCOSE(pid ,timestamp, value, comment):
         print("Connected to SQLite")
 
         sqlite_insert_with_param = """INSERT INTO MEAL
-                          (ID ,TIMESTAMP ,VALUE,COMMENT) 
-                          VALUES (?, ?, ?, ?, ?);"""
+                          (ID ,PERIOD ,VALUE,COMMENT) 
+                          VALUES (?, ?, ?, ?);"""
 
         data_tuple = (pid ,timestamp, value, comment)
         cur.execute(sqlite_insert_with_param, data_tuple)
@@ -69,7 +72,33 @@ def insertIntoGLUCOSE(pid ,timestamp, value, comment):
         if sqliteConnection:
             sqliteConnection.close()
             print("The SQLite connection is closed")
-            
+
+def create_df_pd(pid):
+    """
+    pid = id number
+    """
+    query = """SELECT
+        sleep.start as sleep_start,
+        sleep.end as sleep_end,
+        sleep.quality as quality,
+        meal.start as meal_start,
+        meal.end as meal_end,
+        meal.carbs as carbs,
+        meal.proteins as proteins,
+        meal.fats as fats,
+        glucose.period as period,
+        glucose.value as value
+    FROM
+        SLEEP
+        INNER JOIN MEAL ON SLEEP.id = MEAL.id
+        INNER JOIN GLUCOSE ON MEAL.id = GLUCOSE.id"""
+
+    condition = " WHERE SLEEP.ID = " + str(pid)
+
+    df = pd.read_sql_query(query+condition, con)
+    return df
+
+
 if __name__=='__main__':
   print("main")
   
---
+++ app_local.py
@@ -0,0 +1,136 @@
+import pickle
+from flask import Flask, request, jsonify
+import numpy as np
+import pandas as pd
+import train
+import api_utils as ut
+import sqlite3
+
+#pickle and unpickle must happen with the same version of scikit learn
+
+ml_api = Flask(__name__)
+ml_api.config["DEBUG"] = True
+
+# una route vera, che non sia una richiesta api, ci va se no il deploy va in errore
+@ml_api.route('/')
+def hello_world():
+    return 'Hello, World!!'
+
+@ml_api.route('/train', methods=["GET"])
+def get_pickle():
+    train.training()
+    return 'training'
+
+@ml_api.route('/predict_svc', methods=["POST"])
+def predict_svc():
+    
+    with open('model_svc.pkl', 'rb') as model_svc_pkl:#attenzione a non lasciare fuori dalle funzioni nulla se no i comandi vengono eseguiti
+        model_svc = pickle.load(model_svc_pkl)
+    
+    
+    json_data = request.get_json()# assicurarsi sempre che la modialià e il formato dei dati in invio sia conforme alla modalità e formato di ricezione
+
+    sepal_length = json_data["sepal_length"]
+    sepal_width = json_data["sepal_width"]
+    petal_length = json_data["petal_length"]
+    petal_width = json_data["petal_width"]
+
+    input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = model_svc.predict(input_data)
+    return str(prediction)
+
+
+
+
+@ml_api.route('/predict_rfc', methods=["POST"])
+def predict_rfc():
+    
+    with open('model_rfc.pkl', 'rb') as model_rfc_pkl:
+        model_rfc = pickle.load(model_rfc_pkl)
+    
+    
+    json_data = request.get_json()
+
+    sepal_length = json_data["sepal_length"]
+    sepal_width = json_data["sepal_width"]
+    petal_length = json_data["petal_length"]
+    petal_width = json_data["petal_width"]
+    
+    input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = model_rfc.predict(input_data)
+    return str(prediction)
+
+
+@ml_api.route('/tables/insert/sleep', methods=['POST'])
+def ins_tb_sleep():
+    json_data = request.get_json()
+
+    pid = json_data["pid"]
+    start = json_data["start"]
+    end = json_data["end"]
+    quality = json_data["quality"]
+    comment = json_data["comment"]
+    
+    ut.insertIntoSLEEP(pid ,start ,end,quality,comment)
+    return jsonify('finished_sleep')
+
+@ml_api.route('/tables/insert/meal', methods=['POST'])
+def ins_tb_meal():
+    json_data = request.get_json()
+
+    pid = json_data["pid"]
+    start = json_data["start"]
+    carbs = json_data["carbs"]
+    proteins = json_data["proteins"]
+    fats = json_data["fats"]
+    comment = json_data["comment"]
+    
+    ut.insertIntoFOOD(pid ,start ,carbs,proteins, fats,comment)
+    return jsonify('finished_meal')
+
+@ml_api.route('/tables/insert/glucose', methods=['POST'])
+def ins_tb_glucose():
+    json_data = request.get_json()
+
+    pid = json_data["pid"]
+    timestamp = json_data["timestamp"]
+    value = json_data["value"]
+    comment = json_data["comment"]
+    
+    ut.insertIntoGLUCOSE(pid ,timestamp, value, comment)
+    return jsonify('finished_glucose')
+
+
+@ml_api.route('/tables/createall', methods=["GET"])
+def tables_create():
+    #la creazione del db avviene automaticamente se non esiste 
+    conn = sqlite3.connect('main.db')
+    #datetime format https://stackoverflow.com/questions/17227110/how-do-datetime-values-work-in-sqlite
+    #"YYYY-MM-DD HH:MM:SS.SSS"
+    conn.execute('''CREATE TABLE SLEEP
+         (ID INTEGER     NOT NULL,
+         START           TEXT    NOT NULL,
+         END            TEXT     NOT NULL,
+         QUALITY        INT,
+         COMMENT         VARCHAR(150),
+         PRIMARY KEY ( ID, START, END));''')
+    conn.execute('''CREATE TABLE MEAL
+         (ID INTEGER      NOT NULL,
+         START           TEXT    NOT NULL,
+         CARBS        INT     NOT NULL,
+         PROTEINS        INT,
+         FATS        INT,
+         COMMENT         VARCHAR(150),
+         PRIMARY KEY (ID, START));''')
+    conn.execute('''CREATE TABLE GLUCOSE
+         (ID INTEGER     NOT NULL,
+         PERIOD           TEXT    NOT NULL,
+         VALUE            INT NOT NULL     ,
+         COMMENT         VARCHAR(150),
+         PRIMARY KEY (ID, PERIOD));
+         ''')
+    conn.close()
+    return "Table created successfully"
+
+if __name__ == '__main__':
+    ml_api.run(host='127.0.0.1', port=5000)
