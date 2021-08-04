6b5f9991b880d7de85477b7bcfbdccc19b995d69
bassignana <40897063+bassignana@users.noreply.github.com>
2021-08-04 17:33:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 import numpy as np
 import pandas as pd
 import train
+import api_utils as ut
 
 #pickle and unpickle must happen with the same version of scikit learn
 
@@ -38,6 +39,8 @@ def predict_svc():
     return str(prediction)
 
 
+
+
 @ml_api.route('/predict_rfc', methods=["POST"])
 def predict_rfc():
     
@@ -55,3 +58,81 @@ def predict_rfc():
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     prediction = model_rfc.predict(input_data)
     return str(prediction)
+
+
+@app.route('/tables/insert/sleep', methods=['POST'])
+def ins_tb_sleep():
+    json_data = request.get_json()
+
+    pid = json_data["pid"]
+    start = json_data["start"]
+    end = json_data["end"]
+    quality = json_data["quality"]
+    comment = json_data["comment"]
+    
+    ut.insertIntoSLEEP(pid ,start ,end,quality,comment)
+    return jsonify('finished_sleep')
+
+@app.route('/tables/insert/meal', methods=['POST'])
+def ins_tb_meal():
+    json_data = request.get_json()
+
+    pid = json_data["pid"]
+    start = json_data["start"]
+    end = json_data["end"]
+    carbs = json_data["carbs"]
+    proteins = json_data["proteins"]
+    fats = json_data["fats"]
+    comment = json_data["comment"]
+    
+    ut.insertIntoFOOD(pid ,start ,end,carbs,proteins, fats,comment)
+    return jsonify('finished_meal')
+
+@app.route('/tables/insert/glucose', methods=['POST'])
+def ins_tb_glucose():
+    json_data = request.get_json()
+
+    pid = json_data["pid"]
+    timestamp = json_data["timestamp"]
+    value = json_data["value"]
+    comment = json_data["comment"]
+    
+    ut.insertIntoGLUCOSE(pid ,timestamp, value, comment)
+    return jsonify('finished_glucose')
+
+@ml_api.route('/db/create', methods=["GET"])
+def db_create():
+    
+    conn = sqlite3.connect('main.db')
+    return "Opened or created database successfully
+
+@ml_api.route('/tables/createall', methods=["GET"])
+def tables_create():
+    
+    conn = sqlite3.connect('main.db')
+    #datetime format https://stackoverflow.com/questions/17227110/how-do-datetime-values-work-in-sqlite
+    #"YYYY-MM-DD HH:MM:SS.SSS"
+    conn.execute('''CREATE TABLE SLEEP
+         (ID INT PRIMARY KEY     NOT NULL,
+         START           TEXT    NOT NULL,
+         END            TEXT     NOT NULL,
+         QUALITY        INT,
+         COMMENT         VARCHAR(150));
+         CREATE TABLE MEAL
+         (ID INT PRIMARY KEY     NOT NULL,
+         START           TEXT    ,
+         END            TEXT     ,
+         CARBS        INT     NOT NULL,
+         PROTEINS        INT,
+         FATS        INT,
+         COMMENT         VARCHAR(150));
+         CREATE TABLE GLUCOSE
+         (ID INT PRIMARY KEY     NOT NULL,
+         TIMESTAMP           TEXT    NOT NULL,
+         VALUE            INT NOT NULL     ,
+         COMMENT         VARCHAR(150));
+         ''')
+    conn.close()
+    return "Table created successfully";
+
+
